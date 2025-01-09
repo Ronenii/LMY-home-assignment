3609eba717df554a0d444231170e20b69071c0eb
Ronenii <ronen.glm@gmail.com>
2025-01-09 16:09:44 +0200

    - Added docker compose - Created init.sql for db initialization - Created db health check endpoint from BE

--- BE/src/app.js
+++ BE/src/app.js
@@ -1,13 +1,14 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import task from './route/task.js';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import task from "./route/task.js";
+import pool from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 8000;
-const TASK_ROUTE = process.env.TASK_API_ROUTE || '/api/task';
+const TASK_ROUTE = process.env.TASK_API_ROUTE || "/api/task";
 
 // Middleware
 
@@ -15,22 +16,35 @@ const TASK_ROUTE = process.env.TASK_API_ROUTE || '/api/task';
 app.use(express.json());
 
 // CORS
-app.use(cors()); 
+app.use(cors());
 
 // Health Check
-app.get('/health', (req, res) => {
-    res.status(200).json({ message: "OK" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
+// DB Health Check
+app.get("/db_health", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT current_database()");
+    res.status(200).json({
+      message: `Connected to: ${result.rows[0].current_database}`,
+    });
+  } catch (err) {
+    console.error("Error during query execution:", err);
+  }
 });
 
 // Routes
+
 app.use(TASK_ROUTE, task);
 
 // Handle unmatched routes
-app.use('*', (req, res) => {
-    res.status(404).json({ message: 'Route not found' });
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 // Server running
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
