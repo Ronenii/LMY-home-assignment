906249af7e5a29a85c9e873b767b8de42546b6a4
Ronenii <ronen.glm@gmail.com>
2025-01-09 15:33:38 +0200

    - Refactoring app.js - Added CORS to app.js

--- BE/src/app.js
+++ BE/src/app.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import cors from cors;
-import task from './route/task.js'
+import cors from 'cors';
+import task from './route/task.js';
 
 dotenv.config();
 const app = express();
@@ -9,25 +9,28 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const TASK_ROUTE = process.env.TASK_API_ROUTE || '/api/task';
 
-
 // Middleware
 
-// Json parser
+// JSON Parser
 app.use(express.json());
 
 // CORS
-app.use(cors);
+app.use(cors()); 
+
+// Health Check
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: "OK" });
+});
 
 // Routes
-app.use(TASK_ROUTE, task)
+app.use(TASK_ROUTE, task);
 
-app.use('*', (req, res, next) => {
-    res.status(404).json({
-        message: 'Route not found'
-    })
-})
+// Handle unmatched routes
+app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
 
 // Server running
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
