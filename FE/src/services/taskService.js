0b639cb3572b5ec347ae1e6503f8df51225a019d
Ronenii <ronen.glm@gmail.com>
2025-01-11 16:46:33 +0200

    added task service

---
+++ FE/src/services/taskService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const taskApiUrl =
+  import.meta.env.VITE_BACKEND_URL + import.meta.env.VITE_TASK_URL;
+
+export async function fetchTasks() {
+  console.log("Trying to fetch tasks.");
+  try {
+    const res = await axios.get(taskApiUrl);
+
+    console.log("got polls successfully", res.data);
+    return res.data;
+  } catch (err) {
+    console.log("Error fetching tasks. Error info:", err);
+  }
+}
