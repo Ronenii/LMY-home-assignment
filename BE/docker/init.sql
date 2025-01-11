f2295f6f7a57f848f29d252107fa72a261265c1d
Ronen Gelmanovich <ronenge@mta.ac.il>
2025-01-11 20:53:31 +0200

    Adjusted db and added jsonwebtoken dependency

--- BE/docker/init.sql
+++ BE/docker/init.sql
@@ -1,8 +1,17 @@
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    due_date DATE,
-    status VARCHAR(50) DEFAULT 'open',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE
+    users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL
+    );
+
+CREATE TABLE
+    tasks (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        due_date DATE,
+        status VARCHAR(50) DEFAULT 'open',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        user_id INT REFERENCES users (id)
+    );
