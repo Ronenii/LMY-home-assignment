3c563ed5d5e305924e84ed79e941b1cba215cd70
Ronen Gelmanovich <ronenge@mta.ac.il>
2025-01-11 20:39:06 +0200

    -Created dockerfile -adjusted docker compose -adjusted env in backend to support env vars passed from docker compose

--- BE/src/db/database.js
+++ BE/src/db/database.js
@@ -5,10 +5,17 @@ const { Pool, types } = pkg;
 
 dotenv.config();
 
+const dbHost = process.env.DB_HOST || "localhost"; // Default to localhost
+const dbPort = process.env.DB_PORT || 5432; // Default to 5432
+const dbUser = process.env.DB_USER || "postgres";
+const dbPassword = process.env.DB_PASSWORD || "1";
+const dbName = process.env.DB_NAME || "task_manager";
+
 const connectionString =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL;
+    : `postgresql://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+
 
 if (!connectionString) {
   throw new Error(
